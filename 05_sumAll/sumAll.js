961708dba645894075a794038272806d97c460c1
Fiifo <131212518+Fiifo@users.noreply.github.com>
2024-07-31 18:00:31 -0300

    Exercises done! (7/7)
    Foundation done!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let result = 0;
+    if(n1 < 0 || n2 < 0 || !(Number.isInteger(n1)) || !(Number.isInteger(n2))){
+        return "ERROR"
+    }
+    if(n2 < n1){
+        for(let i = n2; i<= n1; i++){
+            result += i;
+        }
+    }else{
+    for(let i = n1; i <= n2; i++){
+        result += i;
+    }}
+    return result;
 };
 
 // Do not edit below this line
